5eb8112348337f0c26508a895c1295ea31dfaae6
Zack Moore <ormico@users.noreply.github.com>
2016-02-21 19:34:21 -0500

    update gulp task app-css to depend on app-sass; app-css concats css files into './css/all.css' including bootstrap css file

--- NpmGulpBowerWebApp1/gulpfile.js
+++ NpmGulpBowerWebApp1/gulpfile.js
@@ -1,7 +1,9 @@
 ﻿// call require() to obtain a reference to each plugin we need
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    concat = require('gulp-concat'),
+    minifyCss = require('gulp-minify-css');
 
 // Task: default
 // create default task that depends on task app-css
@@ -10,11 +12,23 @@ var gulp = require('gulp'),
 gulp.task('default', ['app-css']);
 
 // Task: app-css
+gulp.task('app-css', ['app-sass'], function (done) {
+    return gulp.src([
+            './bower_components/bootstrap/dist/css/bootstrap.css',
+            './css/**/*.css',
+            '!./css/all.css' // use '!' to exclude 'all.css' when concat all css files into all.css
+    ])
+      .pipe(minifyCss())
+      .pipe(concat('all.css').on('error', sass.logError))
+      .pipe(gulp.dest('./css/'));
+});
+
+// Task: app-css
 // scan all *.scss files under css folder
 // run these files through the sass compiler
 // sending all errors to sass.logError
 // send output of sass compilation to css folder
-gulp.task('app-css', function (done) {
+gulp.task('app-sass', function (done) {
     return gulp.src('./css/**/*.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('./css'));
